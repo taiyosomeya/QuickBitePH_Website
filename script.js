523841cbdc12b33b44c5fd97bbfca03fdd63d761
elijahart7-code <elijahart7@gmail.com>
2025-11-14 22:33:20 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// simple behavior: focus highlight search bar
+const searchBox = document.querySelector(".search-box input");
+
+searchBox.addEventListener("focus", () => {
+    searchBox.style.background = "#fff";
+});
+
+searchBox.addEventListener("blur", () => {
+    searchBox.style.background = "transparent";
+});
